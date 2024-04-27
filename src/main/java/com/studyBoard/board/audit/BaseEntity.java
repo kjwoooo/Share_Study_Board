377963428da7efd96a373d17d61c03b5d074c13b
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-04-27 17:40:04 +0900

    feat:  생성일시 및 수정일시 Auditing 적용

--- src/main/java/com/studyBoard/board/audit/BaseEntity.java
+++ src/main/java/com/studyBoard/board/audit/BaseEntity.java
@@ -3,28 +3,24 @@ package com.studyBoard.board.audit;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @Data
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
-    @Column(name = "CREATED_DATE", nullable = false, updatable = false)
-//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_DATE")
     private LocalDateTime createdDate;
 
     @LastModifiedDate
-    @Column(name = "MODIFIED_DATE", nullable = false)
-//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "MODIFIED_DATE")
     private LocalDateTime modifiedDate;
+
 }
